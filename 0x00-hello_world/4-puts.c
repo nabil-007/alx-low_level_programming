58a6e63d0079eb59ee2ab4a590b7c04a06c563fb
itsme <nabilelbekkali@gmail.com>
2023-03-16 15:12:57 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,6 @@
  */
 int main(void) 
 {
-char myStr[] = "\"Programming is like building a multilingual puzzle";
-puts(myStr);
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
