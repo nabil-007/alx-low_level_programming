c4c42da9224c4dee6d57429012306cc250553f35
itsme <nabilelbekkali@gmail.com>
2023-03-16 15:01:10 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 #include<stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 
 int main(void) 
-
-
 {
 	char myStr[] = "\"Programming is like building a multilingual puzzle";
 	puts(myStr);
