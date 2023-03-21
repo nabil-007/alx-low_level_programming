a67da319b4844630cd711e70039d19cb5b027249
itsme <nabilelbekkali@gmail.com>
2023-03-21 16:22:42 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/** print 10 times alphabets
+ *
+ * retun 0 as succes
+ * */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+	for(i=0 ; i<=10 ; i++)
+	{
+		for(c='a' ; c<='z' ; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
