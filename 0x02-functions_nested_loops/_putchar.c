5787af3e0d2a62467a934aa50c188cf37df6ebc0
itsme <nabilelbekkali@gmail.com>
2023-03-21 13:50:44 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -7,7 +7,7 @@
 * Return: On success 1.
 * On error, -1 is returned, and errno is set appropriately.
 */
-int _putchar(char c)
+int _putchar(char c) /*this function has to return character as output */
 {
 return (write(1, &c, 1));
 }
