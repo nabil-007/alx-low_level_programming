074a80dd848034ece8bbe8557a3be42bfcb3dab8
itsme <nabilelbekkali@gmail.com>
2023-03-21 13:04:51 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,8 +7,14 @@
 */
 int main(void)
 {
-_putchat("_putchar \n");
+_putchat("_");
+_putchat("p");
+_putchat("u");
+_putchat("t");
+_putchat("c");
+_putchat("h");
+_putchat("a");
+_putchat("r");
+_putchat("\n");
 return (0);
 }
-
-
