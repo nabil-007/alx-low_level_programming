fa36ac5889e9ed53faa8850f5bd8c17d38941ebf
itsme <nabilelbekkali@gmail.com>
2023-03-22 09:49:22 -0700

    updated

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -6,20 +6,23 @@
 void print_times_table(int n)
 {
 int i,j,k;
-int n ;
 while( n<=10 && n>0 )
 {
 	for (i=0 ; i <= n ; i++)
 {
-	_putch('0');
+	_putchar('0');
+
 		for( j = 0 ; j <= n ;j++)
 		{
 		_putchar (',');
-		_putchar ('  ');
+		_putchar (' ');
+		_putchar (' ');
 		k=i*j;
 		_putchar (k);
 
 
 		}
 		_putchar('\n');
+}
+}
 }
