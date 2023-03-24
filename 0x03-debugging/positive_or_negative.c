75173bee808802b3a93a5c4c06dbc47658b649e4
itsme <nabilelbekkali@gmail.com>
2023-03-23 17:36:53 -0700

    updated

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * positive_or_negative - check if the number is positif or nit
+ *
+ * @i integer to be checked
+ *
+ * return : always 0.
+ */
+
+void positive_or_negative(int i)
+{
+	if (i >= 0)
+	{
+		printf("%d the number is positif",i);
+	}
+	else
+	{
+		printf("%d the number is negatif",i);
+	}
