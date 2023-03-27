28a566e56d8949dc96586e6b90bc86acb8c6c86c
itsme <nabilelbekkali@gmail.com>
2023-03-27 13:40:31 -0700

    updated

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include<stdio.h>
 
 /**
  * puts2 - prints one char out of 2 of a string
@@ -18,8 +19,8 @@ void puts2(char *str)
 
 	for (i = 0; i < len; i += 2)
 	{
-		_putchar(str[i]);
+		printf("%c", str[i]);
 	}
 
-	_putchar('\n');
+	printf("\n");
 }
