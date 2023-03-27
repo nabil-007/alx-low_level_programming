eec12d1d7477663139b96f8193510fee95631956
itsme <nabilelbekkali@gmail.com>
2023-03-27 10:37:05 -0700

    updated

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * _puts - to print strings
+ * @s : the string to be printed
+ */
+void _puts(char *str)
+{
+	int i=0;
+
+	while(*(str + i) != '\0')
+	{
+		printf("%c",str[i]);
+	}
+}
