c1c67c01a4408c0871b4bf22701d4425e05ae4ae
itsme <nabilelbekkali@gmail.com>
2023-03-29 16:21:53 -0700

    updated

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,28 +6,21 @@
  *
  * return :pointer to  destination
  */
+
 char *_strcat(char *dest, char *src)
 {
-        int i = 0;
-	int j = 0;
-        int len1;
-	int len2;
-	int k;
+	int length;
+	int j;
 
-        while(dest[i] != 0)
-        {
-                i++;
-        }
-        len1 = i - 1;
-        while(src[j] != 0)
-        {
-                j++;
-        }
-        len2 = j;
-        for(k = 0 ; k < len2 ; k++)
-        {
-               dest[k + len1] = src[k + len2];
-        }
-	dest[len2] = '\0';
-return (dest);
+	length = 0;
+	while (dest[length] != '\0')
+	{
+		length++;
+	}
+	for (j = 0; src[j] != '\0'; j++, length++)
+	{
+		dest[length] = src[j];
+	}
+	dest[length] = '\0';
+	return (dest);
 }
