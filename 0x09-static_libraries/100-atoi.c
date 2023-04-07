0d9048a57a4c7f3aabf54d392ffc8262db56a975
itsme <nabilelbekkali@gmail.com>
2023-04-07 14:14:20 -0700

    updated

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -1,12 +1,23 @@
-#include"main.h"
+#include "main.h"
 /**
- *_atoi - functio to
- * @s:string to
+ * _atoi - convert a string to an integer.
+ * @s: pointer to string
  *
- * Return: 1 as succes,0 otherwise
+ * Return: integer value of string, or 0 if no digits are found
  */
-
 int _atoi(char *s)
 {
-	return (1);
+int sign = 1;
+int result = 0;
+while (*s)
+{
+if (*s == '-')
+sign = -sign;
+else if (*s >= '0' && *s <= '9')
+result = result * 10 + (*s - '0');
+else if (result > 0)
+break;
+s++;
+}
+return (sign *result);
 }
