c129a18b630b400dab5617cdf258fbefc062e681
itsme <nabilelbekkali@gmail.com>
2023-04-09 15:13:42 -0700

    updated

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main - function that prints the passed number of agruments
+ * @argv: the number of arguments
+ *@argc: pointer to an array
+ *
+ * Return: 1 on succes
+ */
+int main(int argc, char __attribute__((unused))*argv[])
+{
+	if (argc > 1)
+		printf("%i\n", argc - 1);
+	return (1);
+
+}
