7c121db9ba0bed8c70b0f990b59747423601eb96
itsme <nabilelbekkali@gmail.com>
2023-04-10 06:59:57 -0700

    updated

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,54 +1,56 @@
-#include<stdio.h>
-#include<stdlib.h>
-/**
- * is_digits - function that check there is a character in digits
- * @s: string to check
- *
- * Return: 0 as succes, otherwise 1
- */
-int is_digits(char *s)
-{
-	int i = 0;
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-	while(s[i] != '\0')
-		{
-			if(s[i] >= '0' && s[i] <= '9')
-			{
-				return(0);
-				i++;
-			}
-		}
+/**
+ * is_num - test if it's positive number.
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
+ */
+
+bool is_num(char *argvv)
+{
+	int j = 0;
+
+	for (j = 0; argvv[j]; j++)
+	{
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+			return (0);
+	}
 	return (1);
 }
-	
+
 /**
- * main-programe to add positive numbers
- * @argc:to count args
- * @argv: pointer to array of strings
- *
- * Return:0 as succes 1 in case the entre isn't digit
+ * main -  program that adds positive numbers.
+ * @argc: argument counter
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument wasn't a number
  */
-int main(int argc , char *argv[])
+
+int main(int argc, char *argv[])
 {
+	int i = 1;
 	int sum = 0;
-	int i = 0;
-	if(argc ==1)
+
+	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for (i = 0; i < argc; i++)
+
+	/* check all arguments to add numbers */
+	while (i < argc)
 	{
-		if(is_digits(argv[i]) == 0)
-			{
-				sum += atoi(argv[i]);
-				printf("%i\n", sum);
-			}
+		if (is_num(argv[i]))
+			sum += atoi(argv[i]);
 		else
-			{
+		{
 			printf("Error\n");
 			return (1);
-			}
+		}
+		i++;
 	}
+	printf("%d\n", sum);
+
 	return (0);
 }
