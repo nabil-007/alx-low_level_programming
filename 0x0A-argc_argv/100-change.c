acc415681f7f04a154cbb66081ef5feefd7e2cc0
itsme <nabilelbekkali@gmail.com>
2023-04-10 18:31:58 -0700

    the hardest

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+#include<stdlib.h>
+/**
+ * main- function to print coins changes
+ * @argc: to count length of string
+ * @argv: pointer to string of array
+ *
+ * Return: 0 as succes,1 otherwise
+ */
+int main(int argc, char *argv[])
+{
+	int n;
+	int sum = 0;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	if (atoi(argv[1]) < 0)
+	{
+		printf("0\n");
+	}
+	n = atoi(argv[1]);
+	sum += n / 25;
+	n = n % 25;
+	sum += n / 10;
+	n = n % 10;
+	sum += n / 5;
+	n = n % 5;
+	sum += n / 2;
+	n = n % 2;
+	sum +=  n / 1;
+	printf("%d\n", sum);
+	return (0);
+}
