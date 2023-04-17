30a452671c2269a71e61bc954cb56bea833beef3
itsme <nabilelbekkali@gmail.com>
2023-04-17 07:40:13 -0700

    updated

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_FILE
+#define DOG_FILE
+/**
+ * struct dog - containing the name age of the puppy
+ * @name: the name of the puppy
+ * @age: the age of the puppy
+ * @owner: the owner of the puppy
+ *
+ * Description:  futher description
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif
