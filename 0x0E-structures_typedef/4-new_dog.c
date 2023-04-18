f9c52dd6e8ddd3068e44266ee4506d5c6edee8a8
itsme <nabilelbekkali@gmail.com>
2023-04-17 22:47:06 -0700

    updated

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -16,45 +16,29 @@ dog_t *new_dog(char *name, float age, char *owner)
 	char *ptr;
 
 	if (name == NULL || owner == NULL)
-	{
 		return (NULL);
-	}
 	d = malloc(sizeof(dog_t));
 	if (d == NULL)
-	{
 		return (NULL);
-	}
 	for (len = 0, ptr = name; *ptr; len++)
-	{
 		ptr++;
-	}
 	d->name = malloc(len + 1);
 	if (d->name == NULL)
-	{
 		free(d->name);
 		free(d);
 		return (NULL);
-	}
 	for (len = 0, ptr = owner; *ptr; len++)
-	{
 		ptr++;
-	}
 	d->owner = malloc(len + 1);
 	if (d->owner == NULL)
-	{
 		free(d->owner);
 		free(d);
 		return (NULL);
-	}
 	for (len = 0; *name != '\0'; len++, name++)
-	{
 		d->name[len] = *name;
-	}
 	d->name[len] = '\0';
 	for (len = 0; *owner != '\0'; len++, owner++)
-	{
 		d->owner[len] = *owner;
-	}
 	d->owner[len] = '\0';
 	d->age = age;
 	return (d);
