ed16fbed383b3f6355e218ab1d7171f2c8a4f9b5
itsme <nabilelbekkali@gmail.com>
2023-04-17 18:28:22 -0700

    updated

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -11,6 +11,10 @@ void print_dog(struct dog *d)
 	{
 		printf("(nil)");
 	}
+	if (d == NULL)
+	{
+		printf("");
+	}
 	printf("Name: %s\n", d->name);
 	printf("Age: %f\n", d->age);
 	printf("Owner: %s\n", d->owner);
