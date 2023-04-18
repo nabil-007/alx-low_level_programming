99a2175636759124a627ea898dae97ea938f09c0
itsme <nabilelbekkali@gmail.com>
2023-04-17 19:16:17 -0700

    updated

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -8,12 +8,13 @@
  *
  * Description:  futher description
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	char *owner;
 	float age;
-}dog_t;
+};
+typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif
