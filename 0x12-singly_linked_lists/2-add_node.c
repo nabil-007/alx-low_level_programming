9b9dd3249418fd8d262d3cadd2289399cb4fca8c
itsme <nabilelbekkali@gmail.com>
2023-04-29 01:16:10 -0700

    updated

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,19 +1,6 @@
 #include "lists.h"
-
-/**
- * _strlen - find string length
- * @str: string
- *
- * Return: length
- */
-int _strlen(const char *str)
-{
-	int len;
-
-	for (len = 0; str[len] != '\0'; len++)
-		;
-	return (len);
-}
+#include<string.h>
+#include<stdlib.h>
 
 /**
  * add_node - a function that adds a new node
@@ -37,7 +24,7 @@ list_t *add_node(list_t **head, const char *str)
 		return (NULL);
 
 	new_node->str = strdup(str);
-	new_node->len = _strlen(str);
+	new_node->len = strlen(str);
 
 	if (head == NULL)
 		new_node->next = NULL;
