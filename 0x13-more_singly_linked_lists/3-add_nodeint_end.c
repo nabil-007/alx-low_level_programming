112a5eb10ec4aa5d182a0e1d6df9d105cb65abcf
itsme <nabilelbekkali@gmail.com>
2023-04-30 12:35:33 -0700

    updated

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -10,7 +10,7 @@
  */
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
-	listint_t *new_node, tmp;
+	listint_t *new_node, *tmp;
 
 	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
@@ -18,12 +18,17 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->next =NULL;
-	while (head)
+	new_node->next = NULL;
+	if (*head == NULL)
+	{
+		*head = new_node;
+	}
+	else
 	{
 		tmp = *head;
-		tmp = tmp->next;
+		while (tmp->next != NULL)
+			tmp = tmp->next;
+		tmp->next = new_node;
 	}
-	next = new_node;
 	return (new_node);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,14 @@
 #ifndef LIST_H
 #define LIST_H
 #include<stddef.h>
+#include<stdlib.h>
+#include<string.h>
 
 typedef struct listint_s
 {
 	int n;
 	struct listint_s *next;
-}listint_t;
+} listint_t;
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
