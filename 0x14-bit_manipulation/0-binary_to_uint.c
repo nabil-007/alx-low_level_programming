9820b3360adadad96373b131bf8e1ab3e25af7ca
itsme <nabilelbekkali@gmail.com>
2023-05-05 03:42:50 -0700

    updated

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,23 +8,21 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int c;
+	unsigned int c = 0;
+	int i = 0;
 
 	if (b == NULL)
 	{
 		return (0);
 	}
-	for (; *b != '\0'; b++)
+	while (b[i] != '\0')
 	{
-		if (*b == '0' || *b == '1')
-		{
-			c <<= 1;
-			if (*b == '1')
-				c += 1;
-		}
-		else
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
+		c = c << 1;
+		if ((b[i] & 1) != 0)
+			c += 1;
+		i++;
 	}
 	return (c);
 }
-
